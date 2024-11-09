4d882adfd118f825d7d6a8ce1a9dd7e9252abc8e
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2024-11-09 12:17:43 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "Divyanka"
+let age = 18
+let isLoggedIn = false
+let state;
+
+/*
+number => 2 to power 53
+bigint => large data (used by -> facebook and big companies)
+string => ""
+boolean => true/false
+null => standalone value (object)
+undefined => currently value is not defined  (type) -> typeof(null) = object
+symbol => unique
+
+
+object
+*/
+
+console.log(typeof null);
+console.log(typeof undefined);
+
