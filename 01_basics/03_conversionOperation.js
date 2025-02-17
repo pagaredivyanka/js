b54bf0c81ded3e55ad720ba645fc1c28152fbaed
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2025-02-17 18:00:52 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let score = 99
+// let score = "99"
+// let score = "99abc"  
+let score = null
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"99" => 99
+//"99abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "robot" => true
+
+let someNumber = 99
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
