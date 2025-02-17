6e6338d78c52752243e41148520ec56abf17cac3
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2025-02-17 19:28:13 +0000

    conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,3 +29,43 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+//_____________________Operation______________________
+
+let value = 3
+let negValue = - value
+console.log(negValue); //-3
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 2);
+console.log(2 / 2);
+console.log(2 % 2);
+
+let str1 = "Robot"
+let str2 = "Vinu"
+
+
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(3 + 2 + "3")
+// console.log("1" + 2)
+
+console.log(3 + 4 * 5 % 3);  
+// Bracket
+// Operator
+// Division
+// Multiplication
+// Addition
+// Subtraction
+
+
+
+
+
+
